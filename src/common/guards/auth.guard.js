dd67f6ab1e210381ca1c8659e83e1387d803e056
Javad Yousefi <javadyousefi9000@gmail.com>
2024-06-29 20:46:01 +0330

    add authorized middleware

--- src/common/guards/auth.guard.js
+++ src/common/guards/auth.guard.js
@@ -5,6 +5,24 @@ const jwt = require("jsonwebtoken");
 // user model
 const { userModel } = require("../../modules/user/user.model")
 
+async function isAuthorized(req, res, next) {
+    try {
+        // get user token
+        const token = req?.cookies?.blog_jwt
+        // reject if token is not available
+        if (!token) throw new createError.Unauthorized("login to your account first")
+        const tokenData = await jwt.verify(token, process.env.JWT_SECRET)
+        // check email is in user model or not
+        if ("email" in tokenData) {
+            const userData = await userModel.findOne({ email: tokenData.email })
+            return next()
+        }
+        throw new createError.Unauthorized("user not authorized !")
+    } catch (error) {
+        next(error)
+    }
+}
+
 async function checkIsAdmin(req, res, next) {
     try {
         // get user token
@@ -25,4 +43,4 @@ async function checkIsAdmin(req, res, next) {
     }
 }
 
-module.exports = { checkIsAdmin }
+module.exports = { checkIsAdmin, isAuthorized }
